7bf7e9d60a57179babe8e1cfc8aa289faa2d2dea
achyutrimal4 <achyutrimal4@gmail.com>
2022-11-02 13:39:41 +0545

    users

--- fotomoon/urls.py
+++ fotomoon/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gallery.urls') )
+    path('', include('gallery.urls') ),
+    path('users/', include('users.urls')),
 ]
 
 
